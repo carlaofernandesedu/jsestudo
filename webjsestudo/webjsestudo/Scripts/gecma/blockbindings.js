cdf1de693ee433943006c0ae448fd6004e250f5a
151786 <carlosmelo@sp.gov.br>
2018-08-09 16:05:53 -0300

    Estudo let e const

---
+++ webjsestudo/webjsestudo/Scripts/gecma/blockbindings.js
@@ -0,0 +1,21 @@
+﻿
+var varglobal = 3; 
+
+
+function LetShadowVar() {
+    //sobre escreve a variavel acima var varglobal
+    let varglobal = 4; 
+    return varglobal;
+}
+
+//constante precisa ser inicializada  e nao pode ser alterada
+const xpto maxItens = 30; 
+
+const person = { name: "Nicholas" }; 
+person.name = "Greg"; // funciona;
+
+person = { name: "Greg" };  //binding ao object nao nas suas propriedades nesse caso  prvent modification binding not bound value /(valores associados)
+
+//area especifica para uso de variaveis let e const para serem disponiveis para uso apos sua declaracao
+
+
---
+++ webjsestudo/webjsestudo/Scripts/gecma/licoes.js
@@ -0,0 +1,5 @@
+﻿/*
+  CORE Javascipt - ECMA-262 padrao linguagem ECMAScript   - superset ECAMScript e Node.js core da linguagem - 
+  var - hoisted to the top 
+  va e let  let sobrescreve dentro de uma funcao
+ */
